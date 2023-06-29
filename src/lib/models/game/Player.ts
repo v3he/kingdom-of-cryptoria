8e0d6f604f851e2dd3ba8c9d07192f90ba4c39ab
v3he <pablo@v3he.io>
2023-06-29 17:57:24 +0200

    created checkpoint marks for automatic route

--- src/lib/models/game/Player.ts
+++ src/lib/models/game/Player.ts
@@ -1,5 +1,8 @@
 import Konva from "konva"
 
+const GENERATION_RADIUS: number = 200
+const CHECKPOINT_RADIUS: number = 150 
+
 const animationMetadata = {
   idle: { frames: 18, width: 170, height: 200 },
   blinking: { frames: 18, width: 170, height: 200 },
@@ -32,6 +35,8 @@ export class Player {
     this.sprite = new Konva.Sprite({
       x: stage.width() / 2,
       y: stage.height() / 2,
+      width: 170,
+      height: 200,
       image: imageObj,
       animation: 'idle',
       animations: animations,
@@ -46,54 +51,93 @@ export class Player {
 
     imageObj.src = `/images/players/${ trace }/spritesheet.png`;
 
-    this.createRandomPoints()
+    this.generateRoute(8)
+    this.findNextCheckpoint()
 
   }
 
-  walk(direction) {
-    switch (direction) {
-      case 'ArrowRight':
-        this.sprite.scaleX(1)
-        this.sprite.animation('walking')
-        this.sprite.offsetX(0);
-        this.sprite.x(this.sprite.x() + 2);
-        break
-      case 'ArrowLeft':
-        this.sprite.scaleX(-1)
-        this.sprite.animation('walking')
-        this.sprite.offsetX(170);
-        this.sprite.x(this.sprite.x() - 2);
-        break
-      default:
-        break
-    }
-    this.sprite.start();
+  get position() {
+
+    let { x, y } = this.sprite.position()
+
+    x += 170 / 2 // player width
+    y += 200 - 30 // player height
+
+    return { x, y }
+
   }
 
-  idle() {
-    this.sprite.animation('idle');
-    this.sprite.start();
-  }
+  // walk(direction) {
+  //   switch (direction) {
+  //     case 'ArrowRight':
+  //       this.sprite.scaleX(1)
+  //       this.sprite.animation('walking')
+  //       this.sprite.offsetX(0);
+  //       this.sprite.x(this.sprite.x() + 2);
+  //       break
+  //     case 'ArrowLeft':
+  //       this.sprite.scaleX(-1)
+  //       this.sprite.animation('walking')
+  //       this.sprite.offsetX(170);
+  //       this.sprite.x(this.sprite.x() - 2);
+  //       break
+  //     default:
+  //       break
+  //   }
+  //   this.sprite.start();
+  // }
 
-  createRandomPoints(): void {
+  // idle() {
+  //   this.sprite.animation('idle');
+  //   this.sprite.start();
+  // }
 
-    for (let i = 0; i < 7; i++) {
+  private generateRoute(checkpoints: number): void {
+    while (this.points.length < checkpoints) {
 
-      const x = 100 + Math.random() * (this.stage.width() - 200);
-      const y = this.stage.height() * 0.45 + Math.random() * (this.stage.height() * 0.55 - 100);
+      // point should be with a padding of 100px from left and right
+      // point should be with a padding of 45% top and 150px bottom
+      const potentialPoint = {
+        x: 100 + Math.random() * (this.stage.width() - 200),
+        y: this.stage.height() * 0.45 + Math.random() * (this.stage.height() * 0.55 - 150)
+      }
 
-      this.points.push(new Konva.Circle({
-        x, y,
-        radius: 5,
-        fill: 'yellow',
-        stroke: 'black',
-        strokeWidth: 4,
-      }))
+      const { x, y } = this.position
 
+      if (Math.sqrt(Math.pow(potentialPoint.x - x, 2) + Math.pow(potentialPoint.y - y, 2)) > GENERATION_RADIUS) {
+        this.points.push(new Konva.Circle({
+          ...potentialPoint,
+          radius: 5,
+          fill: 'yellow',
+          stroke: 'black',
+          strokeWidth: 4,
+        }))
+      }
     }
 
     this.stage.getLayers()[0].add(...this.points)
 
+  }
+
+  private findNextCheckpoint(): Konva.Circle {
+    while (true) {
+
+      const potentialDest = this.points[Math.floor(Math.random() * this.points.length)];
+
+      const potentialDestPosition = potentialDest.position()
+      const currentPosition = this.position
+
+      const distance = Math.sqrt(
+        Math.pow(potentialDestPosition.x - currentPosition.x, 2) +
+        Math.pow(potentialDestPosition.y - currentPosition.y, 2)
+      );
+
+      if (distance > CHECKPOINT_RADIUS) {
+        potentialDest.fill('red')
+        return potentialDest
+      }
+
+    }
   }
 
 }
